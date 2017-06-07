11ea4733fe913c8946dc6d41a7c05fdff5a3ae97
ArnimJ <arnimj@outlook.com>
2017-06-07 14:55:08 -0400

    recieve text

---
+++ app/src/main/java/edu/virginia/cs4720aj7eb/textbrowse/SmsReciever.java
@@ -0,0 +1,33 @@
+package edu.virginia.cs4720aj7eb.textbrowse;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.gsm.SmsMessage;
+import android.widget.Toast;
+
+public class SmsReciever extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Bundle bundle = intent.getExtras();
+        SmsMessage[] msgs = null;
+        String str = "";
+        if (bundle != null) {
+            //---retrieve the SMS message received---
+            Object[] pdus = (Object[]) bundle.get("pdus");
+            msgs = new SmsMessage[pdus.length];
+            for (int i = 0; i < msgs.length; i++) {
+                msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                str += "SMS from " + msgs[i].getOriginatingAddress();
+                str += " :";
+                str += msgs[i].getMessageBody().toString();
+                str += "n";
+            }
+            //---display the new SMS message---
+            
+            Toast.makeText(context, str, Toast.LENGTH_LONG).show();
+        }
+
+    }
+}
