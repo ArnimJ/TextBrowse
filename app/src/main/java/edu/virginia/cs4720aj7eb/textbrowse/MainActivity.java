0c7e6f65795a436f92536d6d22eeb33c73a8b7ce
ArnimJ <arnimj@outlook.com>
2017-06-07 13:52:06 -0400

    recieve text

--- app/src/main/java/edu/virginia/cs4720aj7eb/textbrowse/MainActivity.java
+++ app/src/main/java/edu/virginia/cs4720aj7eb/textbrowse/MainActivity.java
@@ -1,7 +1,14 @@
 package edu.virginia.cs4720aj7eb.textbrowse;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.telephony.gsm.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button sendMessage = (Button) findViewById(R.id.sendLink);
+        final TextView wiki = (TextView) findViewById(R.id.editWiki);
+
+        sendMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = wiki.getText().toString();
+                sendSMS(text);
+            }
+        });
     }
+
+    //---sends an SMS message to another device---
+    private void sendSMS(String message)
+    {
+        PendingIntent pi = PendingIntent.getActivity(this, 0,
+                new Intent(this, MainActivity.class), 0);
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage("3604644682", null, message, pi, null);
+    }
+
+
 }
