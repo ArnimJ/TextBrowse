18808bb9b1878598b2d83ba86a39b0900fd10f50
Kevin <kwa6nf@virginia.edu>
2017-06-07 15:38:29 -0400

    added messaging

--- app/src/main/java/edu/virginia/cs4720aj7eb/textbrowse/MainActivity.java
+++ app/src/main/java/edu/virginia/cs4720aj7eb/textbrowse/MainActivity.java
@@ -1,13 +1,64 @@
 package edu.virginia.cs4720aj7eb.textbrowse;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.telephony.gsm.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinner;
+    private static final String[] languages = {"Spanish", "French"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button sendMessage = (Button) findViewById(R.id.sendWikiLink);
+        final TextView wiki = (TextView) findViewById(R.id.editWiki);
+
+        sendMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = wiki.getText().toString();
+                sendSMS(text);
+            }
+        });
+
+        spinner = (Spinner)findViewById(R.id.spinner2);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_item, languages);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "You have clicked" + " " + languages[position], Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    //---sends an SMS message to another device---
+    private void sendSMS(String message)
+    {
+        PendingIntent pi = PendingIntent.getActivity(this, 0,
+                new Intent(this, MainActivity.class), 0);
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage("3604644682", null, message, pi, null);
     }
 }
